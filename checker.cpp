0214c7f7db1caf1fe6e88d2c889feeca24973f21
Aruna Maurya <aruna.maurya12@gmail.com>
2020-08-27 16:05:32 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -6,20 +6,23 @@ float MinSpo2[] = {90};
 float MinRespRate[] = {30};
 float MaxRespRate[] = {95};
 
-bool checkbpm(float bpm){
-  return(bpm>MinBPM[0] && bpm<MaxBPM[0]);
-}
-bool checkspo2(float spo2){
-  return(spo2>MinSpo2[0]);
-}
-bool checkRespRate(float respRate){
-  return(respRate>MinRespRate[0] && respRate<MaxRespRate[0]);
-}
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return (checkbpm(bpm) && checkspo2(spo2) && checkRespRate(respRate));
+// bool checkbpm(float bpm){
+//   return(bpm>MinBPM[0] && bpm<MaxBPM[0]);
+// }
+// bool checkspo2(float spo2){
+//   return(spo2>MinSpo2[0]);
+// }
+// bool checkRespRate(float respRate){
+//   return(respRate>MinRespRate[0] && respRate<MaxRespRate[0]);
+// }
+
+bool vitalsAreOk(float value, float low, float high) {
+  return (value>low && value<high);
 }
 
 int main() {
-  assert(vitalsAreOk(80, 95, 60) == true);
-  assert(vitalsAreOk(60, 90, 40) == false);
+  assert(vitalsAreOk(80, 60, 100) == true);
+  assert(vitalsAreOk(60, 40, 90) == true);
+  assert(vitalsAreOk(3, 40, 90) == false);
+  assert(vitalsAreOk(15, 20, 50) == false);
 }
