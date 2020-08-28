4259c6ce03d71af9f36f55d9c68e29cfa15095f1
Aruna Maurya <aruna.maurya12@gmail.com>
2020-08-28 16:17:17 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -6,23 +6,60 @@ float MinSpo2[] = {90};
 float MinRespRate[] = {30};
 float MaxRespRate[] = {95};
 
-// bool checkbpm(float bpm){
-//   return(bpm>MinBPM[0] && bpm<MaxBPM[0]);
-// }
-// bool checkspo2(float spo2){
-//   return(spo2>MinSpo2[0]);
-// }
-// bool checkRespRate(float respRate){
-//   return(respRate>MinRespRate[0] && respRate<MaxRespRate[0]);
-// }
-
-bool vitalsAreOk(float value, float low, float high) {
-  return (value>low && value<high);
+class AlertInSms{
+  public:
+    void SendAlert(const char* vitalName, const char* Message){
+      std::cout<<"Alert! "+vitalName+"breached";
 }
+class AlertInSound{
+  public:
+    void SendAlert(const char* vitalName, const char* Message){
+      std::cout<<"Alert! "+vitalName+"breached";
+}
+  
+class BpmCheck: public VitalCheck
+{
+  public:
+    bool checkVitalIsOk(float value) override
+    {
+      if(value<MinBPM[0])
+        SendAlert("BPM", "Value less than Min BPM");
+      if(value>MaxBPM[0])
+        SendAlert("BPM", "Value more than Max BMP");
+    }
+};
+  
+class spo2Check: public VitalCheck
+{
+  public:
+    bool checkVitalIsOk(float value) override
+    {
+      if(value<Minspo2[0])
+        SendAlert("Spo2", "Value less than Min Spo2");
+    }
+};
+  
+class RespRateCheck: public VitalCheck
+{
+  public:
+    bool checkVitalIsOk(float value) override
+    {
+      if(value<MinRespRate[0])
+        SendAlert("Respiration Rate", "Value less than Min RespRate");
+      if(value>MaxRespRate[0])
+        SendAlert("Respiration Rate", "Value more than Max RespRate");
+    }
+};
+class VitalCheck{
+  public:
+  virtual bool checkVitalIsOk(float value) = 0;
+};
 
 int main() {
-  assert(vitalsAreOk(80, 60, 100) == true);
-  assert(vitalsAreOk(60, 40, 90) == true);
-  assert(vitalsAreOk(3, 40, 90) == false);
-  assert(vitalsAreOk(15, 20, 50) == false);
+  VitalCheck vitalcheck = new VitalCheck();
+  
+  assert(checkVitalIsOk(80, 60, 100) == true);
+  assert(checkVitalIsOk(60, 40, 90) == true);
+  assert(checkVitalIsOk(3, 40, 90) == false);
+  assert(checkVitalIsOk(15, 20, 50) == false);
 }
